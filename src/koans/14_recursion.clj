555acb94fd9ff91ea26d34b814be9523490fb0bf
Carl Huffmeier <carl.huffmeier@glovoapp.com>
2019-08-03 18:52:12 +0200

    Medidate on recursion

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,32 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [remaining coll
+         reversed ()]
+    (if (empty? remaining)
+      reversed
+      (recur (rest remaining) (conj reversed (first remaining))))))
+
+(defn recursive-reverse-reduce [coll]
+  (reduce conj '() coll))
 
 (defn factorial [n]
-  __)
+  (loop [i      n
+         result 1]
+    (if (<= i 1)
+      result
+      (recur (dec i) (* result i)))))
 
 (meditations
   "Recursion ends with a base case"
